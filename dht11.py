c3933a059ff62bd4ede281d672111d566c8efc98
Jackson <roachjackson@gmail.com>
2017-05-22 00:22:27 +0000

    Add files via upload

---
+++ dht11.py
@@ -0,0 +1,142 @@
+#DHT11 Temp and Humidity Sensor 
+
+import RPi.GPIO as GPIO
+import time
+from datetime import datetime
+import tweepy
+
+API_KEY = 'vP28AnmyCD29SaMTecreijLFo'
+API_SECRET = 'zbHl6f5a3YljPOPwLmLE9NwXkoNvlslnDqMga2fbtyLZRly7fU'
+ACCESS_TOKEN = '865800834235342850-HoJkGqcHbckVoEDT2z0Qe9CgEAlHxYB'
+ACCESS_TOKEN_SECRET = 'YNvFKTJ6sCV69vwD1AnyxUaNSISpYEg2VbhgVSOdh67Mg'
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+DHTPIN = 17
+
+GPIO.setmode(GPIO.BCM)
+
+MAX_UNCHANGE_COUNT = 100
+
+STATE_INIT_PULL_DOWN = 1
+STATE_INIT_PULL_UP = 2
+STATE_DATA_FIRST_PULL_DOWN = 3
+STATE_DATA_PULL_UP = 4
+STATE_DATA_PULL_DOWN = 5
+
+def read_dht11_dat():
+	GPIO.setup(DHTPIN, GPIO.OUT)
+	GPIO.output(DHTPIN, GPIO.HIGH)
+	time.sleep(2)
+	GPIO.output(DHTPIN, GPIO.LOW)
+	time.sleep(0.02)
+	GPIO.setup(DHTPIN, GPIO.IN, GPIO.PUD_UP)
+
+	unchanged_count = 0
+	last = -1
+	data = []
+	while True:
+		current = GPIO.input(DHTPIN)
+		data.append(current)
+		if last != current:
+			unchanged_count = 0
+			last = current
+		else:
+			unchanged_count += 1
+			if unchanged_count > MAX_UNCHANGE_COUNT:
+				break
+
+	state = STATE_INIT_PULL_DOWN
+
+	lengths = []
+	current_length = 0
+
+	for current in data:
+		current_length += 1
+
+		if state == STATE_INIT_PULL_DOWN:
+			if current == GPIO.LOW:
+				state = STATE_INIT_PULL_UP
+			else:
+				continue
+		if state == STATE_INIT_PULL_UP:
+			if current == GPIO.HIGH:
+				state = STATE_DATA_FIRST_PULL_DOWN
+			else:
+				continue
+		if state == STATE_DATA_FIRST_PULL_DOWN:
+			if current == GPIO.LOW:
+				state = STATE_DATA_PULL_UP
+			else:
+				continue
+		if state == STATE_DATA_PULL_UP:
+			if current == GPIO.HIGH:
+				current_length = 0
+				state = STATE_DATA_PULL_DOWN
+			else:
+				continue
+		if state == STATE_DATA_PULL_DOWN:
+			if current == GPIO.LOW:
+				lengths.append(current_length)
+				state = STATE_DATA_PULL_UP
+			else:
+				continue
+	if len(lengths) != 40:
+		print "Data not good, skip"
+		return False
+
+	shortest_pull_up = min(lengths)
+	longest_pull_up = max(lengths)
+	halfway = (longest_pull_up + shortest_pull_up) / 2
+	bits = []
+	the_bytes = []
+	byte = 0
+
+	for length in lengths:
+		bit = 0
+		if length > halfway:
+			bit = 1
+		bits.append(bit)
+#	print "bits: %s, length: %d" % (bits, len(bits))
+	for i in range(0, len(bits)):
+		byte = byte << 1
+		if (bits[i]):
+			byte = byte | 1
+		else:
+			byte = byte | 0
+		if ((i + 1) % 8 == 0):
+			the_bytes.append(byte)
+			byte = 0
+#	print the_bytes
+	checksum = (the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3]) & 0xFF
+	if the_bytes[4] != checksum:
+		print "Data not good, skip"
+		return False
+
+	return the_bytes[0], the_bytes[2]
+
+def main():
+#	print "Raspberry Pi wiringPi DHT11 Temperature test program\n"
+#	while True:
+		result = read_dht11_dat()
+		if result:
+			humidity, celsius = result
+			temperature = ((celsius * 9) / 5) + 32 
+			print "Humidity: %s%%,  Temperature: %s F`" % (humidity, temperature)
+
+			temperature = str(temperature)
+			humidity = str(humidity)
+#			time = datetime.now().strftime('%-I:%M')
+			api.update_status("Apartment Conditions: " + temperature + " degrees and " + humidity + "% humidity")
+#		time.sleep(1)
+
+def destroy():
+	GPIO.cleanup()
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+destroy()
